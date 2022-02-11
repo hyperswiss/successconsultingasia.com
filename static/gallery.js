1b343c04bfe4179805875328f82dc57161d9b395
Jean-Maurice Bertrand <76576996+hyperswiss@users.noreply.github.com>
2022-02-11 16:02:42 +0700

    Add files via upload

--- static/gallery.js
+++ static/gallery.js
@@ -1,14 +1,15 @@
-document.write('\
-\
-<section class="pictures-section" id="pictures-section">\
-    <div class="grid_pictures">\
-        <div class="pics"><img src="../img/charlie-costello-YVMINsxTIKY-unsplash.webp" alt="Charlie Costello photo"></div>\
-        <div class="pics"><img src="../img/mega-caesaria-CH7JI7pwVcU-unsplash.webp" alt="Mega Cesaria photo"></div>\
-        <div class="pics"><img src="../img/nick-fewings-_dYoARMMtQw-unsplash.webp" alt="Nick Fewings photo"></div>\
-        <div class="pics"><img src="../img/ammie-ngo-vcu-OZBxxRk-unsplash.webp" alt="Ammie Ngo photo"></div>\
-        <div class="pics"><img src="../img/vicky-t-EY3tC81nFt0-unsplash.webp" alt="Vicky T photo"></div>\
-        <div class="pics"><img src="../img/rowan-heuvel-PgSxHidgJHQ-unsplash.webp" alt="Rowan Heuvel photo"></div>\
-    </div>\
-</section>\
-\
-')
+async function myGallery() {
+    document.write('\
+    \
+    <section class="pictures-section" id="pictures-section">\
+        <div class="grid_pictures">\
+            <div class="pics"><img src="../img/charlie-costello-YVMINsxTIKY-unsplash.webp" alt="Charlie Costello photo"></div>\
+            <div class="pics"><img src="../img/mega-caesaria-CH7JI7pwVcU-unsplash.webp" alt="Mega Cesaria photo"></div>\
+            <div class="pics"><img src="../img/nick-fewings-_dYoARMMtQw-unsplash.webp" alt="Nick Fewings photo"></div>\
+            <div class="pics"><img src="../img/ammie-ngo-vcu-OZBxxRk-unsplash.webp" alt="Ammie Ngo photo"></div>\
+            <div class="pics"><img src="../img/vicky-t-EY3tC81nFt0-unsplash.webp" alt="Vicky T photo"></div>\
+            <div class="pics"><img src="../img/rowan-heuvel-PgSxHidgJHQ-unsplash.webp" alt="Rowan Heuvel photo"></div>\
+        </div>\
+    </section>\
+    \
+    ');}
